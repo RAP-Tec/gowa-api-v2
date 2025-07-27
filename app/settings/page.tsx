1408781292e2d1e368e94b89804478bbfa306f7b
Rogerio Amaral <rogerio@sistemaaula.com.br>
2025-07-27 11:07:49 -0300

    UPD - data.json ignorar

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -103,7 +103,7 @@ export default function SettingsPage() {
       </button>
 
       <h1 className="text-2xl font-bold mb-6">Gowa Platform | Devices and Hooks Dashboard</h1>
-      <h3 className="text-2xl font-bold mb-6">Webhook URL: <spam id="webhookUrl" name="webhookUrl">https://dsfdsfd.com.br/webhook/2323</spam></h3>
+      <h3 className="text-1xl mb-6">Sua Webhook URL para API Oficial: <spam id="webhookUrl" name="webhookUrl">https://dsfdsfd.com.br/webhook/2323</spam></h3>
       
       {/* CRUD HOOKS */}
       <div className="mb-8">
